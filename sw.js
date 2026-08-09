559739cf0e5b24a0bdf02663daf859468b904045
montbaby23 <montbaby23@gmail.com>
2026-08-09 22:22:05 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+
+const CACHE='montbaby-v2';
+const CORE=['./','index.html','style.css','app.js','manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
